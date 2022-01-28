f8a6f4782acacbc16c6786ddccdfc38a40dad077
Matt Johnson <matt.johnson.dev@gmail.com>
2022-01-27 22:48:09 -0700

    Mode-X sprite fix. Raw file now requires width and height as shorts first. Python mode-X file now adds width/height.

--- tools/modex_convert.py
+++ tools/modex_convert.py
@@ -1,30 +1,70 @@
+import sys
+import os
+import struct
 from PIL import Image
 
-im = Image.open('test.bmp')
-px = im.load()
-raw_pal = im.getpalette()
 
-im2 = Image.new(mode="P", size=im.size)
-im2.putpalette(raw_pal)
-px2 = im2.load()
+def run(_filename):
 
-o2 = 0
+    try:
+        im = Image.open(_filename)
+    except FileNotFoundError:
+        print("Error: File not found")
+        return
 
-with open("img.raw", "wb") as f:
-    for plane in range(0, 4):
-        for j in range(0, im.size[1]):
-            for i in range(0, im.size[0] >> 2):
+    if im.mode != 'P':
+        print("Error: Image must be indexed color. Mode is ", im.mode)
+        return
 
-                src_x = i * 4 + plane
-                src_y = j
+    filename, file_extension = os.path.splitext(_filename)
 
-                dst_x = o2 % im2.size[0]
-                dst_y = divmod(o2, im2.size[0])[0]
+    if (im.size[0] & 3) != 0:
+        print("Error: Image width must be a multiple of 4.")
+        return
 
-                px2[dst_x, dst_y] = px[src_x, src_y]
-                f.write(bytes([px[src_x, src_y]]))
+    # print(filename, file_extension)
 
-                o2 += 1
+    out_filename = "{}.raw".format(filename)
 
-    im2.show()
-    f.close()
+    print("Output: {}, {}x{}".format(out_filename, im.size[0], im.size[1]))
+
+    px = im.load()
+    raw_pal = im.getpalette()
+
+    im2 = Image.new(mode="P", size=im.size)
+    im2.putpalette(raw_pal)
+    px2 = im2.load()
+
+    o2 = 0
+
+    with open(out_filename, "wb") as f:
+        f.write(struct.pack("<h", im.size[0]))
+        f.write(struct.pack("<h", im.size[1]))
+
+        for plane in range(0, 4):
+            for j in range(0, im.size[1]):
+                for i in range(0, im.size[0] >> 2):
+                    src_x = i * 4 + plane
+                    src_y = j
+
+                    dst_x = o2 % im2.size[0]
+                    dst_y = divmod(o2, im2.size[0])[0]
+
+                    px2[dst_x, dst_y] = px[src_x, src_y]
+                    f.write(bytes([px[src_x, src_y]]))
+
+                    o2 += 1
+
+        # im2.show()
+        f.close()
+
+
+if __name__ == '__main__':
+    # run()
+    if len(sys.argv) != 2:
+        print("./modex_convert.py foo.bmp")
+        exit(1)
+
+    run(sys.argv[1])
+
+    exit(0)
