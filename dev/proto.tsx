9b6bbbbbbac7693d38d76da37b7a2c396f69f88c
Matt Johnson <matt.johnson.dev@gmail.com>
2022-03-17 21:22:27 -0600

    New assets for prototype

---
+++ dev/proto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="proto" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="../proto.bmp" trans="00fdff" width="256" height="192"/>
+</tileset>
