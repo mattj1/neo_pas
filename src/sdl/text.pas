55cfbf00dff56e5c1854fc7ed2e015cc9f3a284c
Matt Johnson <matt.johnson.dev@gmail.com>
2022-12-11 10:07:06 -0700

    Cleanups

--- src/sdl/text.pas
+++ src/sdl/text.pas
@@ -66,67 +66,32 @@ begin
   fg_p := fg;
   bg_p := bg;
 
-
-  dstRect := SDL_RectCreate(0, 0, 640, 400);
-
-  SDL_RenderClear(sdlRenderer);
-  //SDL_FillRect(indexedBackbuffer, @dstRect, 0);
-
-  //SDL_FillRect(indexedBackbuffer, @dstRect, 100);
-
-  for y := 0 to text_screen_height - 1 do begin
-    for x := 0 to text_screen_width - 1 do begin
-
-        row := bp^ div 32;
-        col := bp^ mod 32;
-        SDL_SetTextureColorMod(fontTexture, pal[fg_p^][0], pal[fg_p^][1], pal[fg_p^][2]);
-
-        srcRect := SDL_RectCreate(col * 8, row * 16, 8, 16);
-        dstRect := SDL_RectCreate(x * 8, y * 16, 8, 16);
-
-
-        SDL_RenderCopyEx(sdlRenderer, fontTexture, @srcRect, @dstRect, 0, @point, 0);
-
-
-        Inc(bp);
-        Inc(fg_p);
-        Inc(bg_p);
-      end;
-    end;
-  //
-
-
-
-
-    SDL_SetTextureColorMod(fontTexture, 255, 255, 0);
-    dstRect.x:=dstRect.x + 4;
-    SDL_RenderCopyEx(sdlRenderer, fontTexture, @srcRect, @dstRect, 0, @point, 0);
-
-    SDL_RenderPresent(sdlRenderer);
-
-    Exit;
-
-  SDL_SetPixelFormatPalette(fontImage^.surface^.format, currentPalette.sdlPalette);
-  SDL_BlitSurface(fontImage^.surface, @srcRect, indexedBackbuffer, @dstRect);
-
-  { --------------- }
-
-  if SDL_SetSurfacePalette(indexedBackbuffer, currentPalette.sdlPalette) <> 0 then
-  begin
-    Halt;
-  end;
-
-  texture := SDL_CreateTextureFromSurface(sdlRenderer, indexedBackbuffer);
-
-  srcRect := SDL_RectCreate(0, 0, indexedBackbuffer^.w, indexedBackbuffer^.h);
   dstRect := SDL_RectCreate(0, 0, sdlWindow1^.w, sdlWindow1^.h);
 
   SDL_RenderClear(sdlRenderer);
-  if SDL_RenderCopy(sdlRenderer, texture, @srcRect, @dstRect) <> 0 then halt;
+
+  for y := 0 to text_screen_height - 1 do
+  begin
+    for x := 0 to text_screen_width - 1 do
+    begin
+
+      row := bp^ div 32;
+      col := bp^ mod 32;
+      SDL_SetTextureColorMod(fontTexture, pal[fg_p^][0], pal[fg_p^][1], pal[fg_p^][2]);
+
+      srcRect := SDL_RectCreate(col * 8, row * 16, 8, 16);
+      dstRect := SDL_RectCreate(x * 8, y * 16, 8, 16);
+
+
+      SDL_RenderCopyEx(sdlRenderer, fontTexture, @srcRect, @dstRect, 0, @point, 0);
+
+      Inc(bp);
+      Inc(fg_p);
+      Inc(bg_p);
+    end;
+  end;
 
   SDL_RenderPresent(sdlRenderer);
-
-  SDL_DestroyTexture(texture);
 end;
 
 procedure TextWriteRaw(ch, color: byte; o: integer);
@@ -142,7 +107,6 @@ begin
   Inc(bg_p, o);
   bp^ := ch;
   fg_p^ := color and $f;
-
 
 end;
 
@@ -237,23 +201,71 @@ begin
   SDL_SetHint(SDL_HINT_RENDER_VSYNC, '1');
 
   i := 0;
-  pal[i][0] := 0; pal[i][1] := 0; pal[i][2] := 0; Inc(i);
-  pal[i][0] := 34; pal[i][1] := 52; pal[i][2] := 209; Inc(i);
-  pal[i][0] := 68; pal[i][1] := 170; pal[i][2] := 204; Inc(i);
-  pal[i][0] := 138; pal[i][1] := 54; pal[i][2] := 34; Inc(i);
-  pal[i][0] := 92; pal[i][1] := 46; pal[i][2] := 120; Inc(i);
-  pal[i][0] := 170; pal[i][1] := 92; pal[i][2] := 61; Inc(i);
-  pal[i][0] := 181; pal[i][1] := 181; pal[i][2] := 181; Inc(i);
-  pal[i][0] := 127; pal[i][1] := 127; pal[i][2] := 127; Inc(i);
+  pal[i][0] := 0;
+  pal[i][1] := 0;
+  pal[i][2] := 0;
+  Inc(i);
+  pal[i][0] := 34;
+  pal[i][1] := 52;
+  pal[i][2] := 209;
+  Inc(i);
+  pal[i][0] := 68;
+  pal[i][1] := 170;
+  pal[i][2] := 204;
+  Inc(i);
+  pal[i][0] := 138;
+  pal[i][1] := 54;
+  pal[i][2] := 34;
+  Inc(i);
+  pal[i][0] := 92;
+  pal[i][1] := 46;
+  pal[i][2] := 120;
+  Inc(i);
+  pal[i][0] := 170;
+  pal[i][1] := 92;
+  pal[i][2] := 61;
+  Inc(i);
+  pal[i][0] := 181;
+  pal[i][1] := 181;
+  pal[i][2] := 181;
+  Inc(i);
+  pal[i][0] := 127;
+  pal[i][1] := 127;
+  pal[i][2] := 127;
+  Inc(i);
 
-  pal[i][0] := 94; pal[i][1] := 96; pal[i][2] := 110; Inc(i);
-  pal[i][0] := 76; pal[i][1] := 129; pal[i][2] := 251; Inc(i);
-  pal[i][0] := 108; pal[i][1] := 217; pal[i][2] := 71; Inc(i);
-  pal[i][0] := 123; pal[i][1] := 226; pal[i][2] := 249; Inc(i);
-  pal[i][0] := 235; pal[i][1] := 138; pal[i][2] := 96; Inc(i);
-  pal[i][0] := 226; pal[i][1] := 61; pal[i][2] := 105; Inc(i);
-  pal[i][0] := 255; pal[i][1] := 217; pal[i][2] := 63; Inc(i);
-  pal[i][0] := 255; pal[i][1] := 255; pal[i][2] := 255; Inc(i);
+  pal[i][0] := 94;
+  pal[i][1] := 96;
+  pal[i][2] := 110;
+  Inc(i);
+  pal[i][0] := 76;
+  pal[i][1] := 129;
+  pal[i][2] := 251;
+  Inc(i);
+  pal[i][0] := 108;
+  pal[i][1] := 217;
+  pal[i][2] := 71;
+  Inc(i);
+  pal[i][0] := 123;
+  pal[i][1] := 226;
+  pal[i][2] := 249;
+  Inc(i);
+  pal[i][0] := 235;
+  pal[i][1] := 138;
+  pal[i][2] := 96;
+  Inc(i);
+  pal[i][0] := 226;
+  pal[i][1] := 61;
+  pal[i][2] := 105;
+  Inc(i);
+  pal[i][0] := 255;
+  pal[i][1] := 217;
+  pal[i][2] := 63;
+  Inc(i);
+  pal[i][0] := 255;
+  pal[i][1] := 255;
+  pal[i][2] := 255;
+  Inc(i);
 
   indexedBackbuffer := SDL_CreateRGBSurface(SDL_SWSURFACE, window_width,
     window_height, 8, 0, 0, 0, 0);
@@ -262,15 +274,16 @@ begin
 
   fontImage := Image_Load('font.bmp');
 
-  fontTexture:= SDL_CreateTextureFromSurface(sdlRenderer, fontImage^.surface);
+  fontTexture := SDL_CreateTextureFromSurface(sdlRenderer, fontImage^.surface);
 
   FillByte(scrbuf^, num_chars, 0);
   FillByte(fg^, num_chars, 0);
   FillByte(bg^, num_chars, 0);
 
   i := 0;
-  for i := 0 to 15 do begin
-      TextWriteRaw(65 + i, i, i);
+  for i := 0 to 15 do
+  begin
+    TextWriteRaw(65 + i, i, i);
   end;
 
   DrawString(10, 10, 'Hello');
